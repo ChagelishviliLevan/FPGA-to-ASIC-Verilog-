f913aabe8aa05bdff1f205e4672f219f086feba6
ChagelishviliLevan <levanichagelishvili@gmail.com>
2026-09-07 13:22:15 +0400

    Fixed project name

--- test/tb.v
+++ test/tb.v
@@ -29,7 +29,7 @@ module tb ();
 
   // Replace tt_um_example with your module name:
   // tt_um_example user_project (
-  Tbilisi_CORDIC_Engine (
+  Tbilisi_CORDIC_Engine user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
